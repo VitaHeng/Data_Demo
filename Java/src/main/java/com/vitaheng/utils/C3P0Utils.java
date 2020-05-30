b793ab01f5a726a4285eba7d002de333f11b6fea
VitaHeng <ai.heng.dev@gmail.com>
2020-05-30 18:04:18 +0800

    dbutils_demo

---
+++ Java/src/main/java/com/vitaheng/utils/C3P0Utils.java
@@ -0,0 +1,76 @@
+package com.vitaheng.utils;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class C3P0Utils {
+    private C3P0Utils() {}
+    private static DataSource ds = new ComboPooledDataSource();
+
+    public static DataSource getDataSource() {
+        return ds;
+    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void release(Connection conn, Statement stat, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+                rs = null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stat != null) {
+                    stat.close();
+                    stat = null;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (conn != null) {
+                        conn.close();
+                        conn = null;
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void release(Connection conn, Statement stat) {
+        try {
+            if (stat != null) {
+                stat.close();
+                stat = null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                    conn = null;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ Java/src/main/java/com/vitaheng/utils/DruidUtils.java
@@ -0,0 +1,89 @@
+package com.vitaheng.utils;
+
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+
+import javax.sql.DataSource;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DruidUtils {
+    private DruidUtils() {}
+    private static DataSource ds;
+
+    static {
+        Properties ps = new Properties();
+        try {
+            ps.load(new FileReader("E:\\study\\Demo\\Data_Demo\\Java\\src\\main\\resources\\config.properties"));
+            ds = DruidDataSourceFactory.createDataSource(ps);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DataSource getDataSource() {
+        return ds;
+    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void release(Connection conn, Statement stat, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+                rs = null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stat != null) {
+                    stat.close();
+                    stat = null;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (conn != null) {
+                        conn.close();
+                        conn = null;
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void release(Connection conn, Statement stat) {
+        try {
+            if (stat != null) {
+                stat.close();
+                stat = null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                    conn = null;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
