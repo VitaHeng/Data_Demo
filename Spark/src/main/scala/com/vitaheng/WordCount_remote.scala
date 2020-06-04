23c1df539d035ba84d095ab30c70a359d1a3815e
VitaHeng <ai.heng.dev@gmail.com>
2020-06-04 18:29:00 +0800

    spark_demo

---
+++ Spark/src/main/scala/com/vitaheng/WordCount_remote.scala
@@ -0,0 +1,22 @@
+package com.vitaheng
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object WordCount_remote {
+
+  def main(args: Array[String]){
+    val conf = new SparkConf()
+      .setMaster("spark://hadoop102:7077")
+      .setAppName("wc_remote")
+      .setJars(List("E:\\Demo\\Data_Demo\\Spark\\target\\Spark-1.0-SNAPSHOT-jar-with-dependencies.jar"))
+      .setIfMissing("spark.driver.host","192.168.213.128")
+      .set("spark.driver.bindAddress","127.0.0.1")
+    val sc = new SparkContext(conf)
+
+    sc.textFile("hdfs://hadoop102:9000/RELEASE").flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_,1).sortBy(_._2,false).saveAsTextFile("E:\\study\\out_remote")
+
+    sc.stop()
+
+  }
+
+}
